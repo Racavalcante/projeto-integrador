f03a6b09d434a79abb8c3f5957ead3c06bebbf9f
racavalcante <rafael.cavalcante@mercadolivre.com>
2022-08-15 14:07:09 -0300

    feat: create a Dto for the output of routes that get cart and discount them

---
+++ src/main/java/dh/meli/projeto_integrador/dto/dtoOutput/DiscountedCartDto.java
@@ -0,0 +1,35 @@
+package dh.meli.projeto_integrador.dto.dtoOutput;
+
+import lombok.*;
+
+/**
+ * Method Getter implemented by Lombok lib for get access the private attributes of the Product Class
+ */
+@Getter
+/**
+ * Method Setter implemented by Lombok lib for set the private attributes of the Product Class
+ */
+@Setter
+/**
+ * Method Default Constructor implemented by Lombok lib
+ */
+@NoArgsConstructor
+/**
+ * Method Constructor with all arguments implemented by Lombok lib
+ */
+@AllArgsConstructor
+/**
+ * Method builder implemented by Lombok lib
+ */
+@Builder
+/**
+ * Class used to create a Data Transfer Object for used on CartOutputDto POJO
+ * @author Rafael Cavalcante
+ * @version 0.0.1
+ * @see java.lang.Object
+ */
+public class DiscountedCartDto {
+    private CartOutputDto cart;
+    private String discount;
+    private Double newPrice;
+}
